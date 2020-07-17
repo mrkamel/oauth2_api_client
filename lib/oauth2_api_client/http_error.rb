4fb729945b10576fe0c9d53e1c84f39b7974b425
Benjamin Vetter <benjamin.vetter@wlw.de>
2020-07-17 21:32:44 +0200

    Docs extended

--- lib/oauth2_api_client/http_error.rb
+++ lib/oauth2_api_client/http_error.rb
@@ -1,4 +1,15 @@
 class Oauth2ApiClient
+  # The HttpError class is the main exception class of Oauth2ApiClient and is
+  # raised when a request fails with some status code other than 2xx. Using
+  # the exception object, you still have access to the response.
+  #
+  # @example
+  #   begin
+  #     client.post("/orders", json: { address: "..." })
+  #   rescue Oauth2ApiClient::HttpError => e
+  #     e.response # => HTTP::Response
+  #   end
+
   class HttpError < StandardError
     attr_reader :response
 
