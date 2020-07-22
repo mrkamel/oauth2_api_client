f77367067341683d142f6a913ff0c63566c66eb9
Benjamin Vetter <benjamin.vetter@wlw.de>
2020-07-22 23:57:46 +0200

    Fix linter

--- lib/oauth2_api_client/http_error.rb
+++ lib/oauth2_api_client/http_error.rb
@@ -17,49 +17,49 @@ class Oauth2ApiClient
 
   class HttpError < StandardError
     STATUSES = {
-      400 => 'Bad Request',
-      401 => 'Unauthorized',
-      402 => 'Payment Required',
-      403 => 'Forbidden',
-      404 => 'Not Found',
-      405 => 'Method Not Allowed',
-      406 => 'Not Acceptable',
-      407 => 'Proxy Authentication Required',
-      408 => 'Request Timeout',
-      409 => 'Conflict',
-      410 => 'Gone',
-      411 => 'Length Required',
-      412 => 'Precondition Failed',
-      413 => 'Payload Too Large',
-      414 => 'URI Too Long',
-      415 => 'Unsupported Media Type',
-      416 => 'Range Not Satisfiable',
-      417 => 'Expectation Failed',
-      418 => 'I\'m A Teapot',
-      421 => 'Too Many Connections From This IP',
-      422 => 'Unprocessable Entity',
-      423 => 'Locked',
-      424 => 'Failed Dependency',
-      425 => 'Unordered Collection',
-      426 => 'Upgrade Required',
-      428 => 'Precondition Required',
-      429 => 'Too Many Requests',
-      431 => 'Request Header Fields Too Large',
-      449 => 'Retry With',
-      450 => 'Blocked By Windows Parental Controls',
+      400 => "Bad Request",
+      401 => "Unauthorized",
+      402 => "Payment Required",
+      403 => "Forbidden",
+      404 => "Not Found",
+      405 => "Method Not Allowed",
+      406 => "Not Acceptable",
+      407 => "Proxy Authentication Required",
+      408 => "Request Timeout",
+      409 => "Conflict",
+      410 => "Gone",
+      411 => "Length Required",
+      412 => "Precondition Failed",
+      413 => "Payload Too Large",
+      414 => "URI Too Long",
+      415 => "Unsupported Media Type",
+      416 => "Range Not Satisfiable",
+      417 => "Expectation Failed",
+      418 => "I'm A Teapot",
+      421 => "Too Many Connections From This IP",
+      422 => "Unprocessable Entity",
+      423 => "Locked",
+      424 => "Failed Dependency",
+      425 => "Unordered Collection",
+      426 => "Upgrade Required",
+      428 => "Precondition Required",
+      429 => "Too Many Requests",
+      431 => "Request Header Fields Too Large",
+      449 => "Retry With",
+      450 => "Blocked By Windows Parental Controls",
 
-      500 => 'Internal Server Error',
-      501 => 'Not Implemented',
-      502 => 'Bad Gateway',
-      503 => 'Service Unavailable',
-      504 => 'Gateway Timeout',
-      505 => 'HTTP Version Not Supported',
-      506 => 'Variant Also Negotiates',
-      507 => 'Insufficient Storage',
-      508 => 'Loop Detected',
-      509 => 'Bandwidth Limit Exceeded',
-      510 => 'Not Extended',
-      511 => 'Network Authentication Required'
+      500 => "Internal Server Error",
+      501 => "Not Implemented",
+      502 => "Bad Gateway",
+      503 => "Service Unavailable",
+      504 => "Gateway Timeout",
+      505 => "HTTP Version Not Supported",
+      506 => "Variant Also Negotiates",
+      507 => "Insufficient Storage",
+      508 => "Loop Detected",
+      509 => "Bandwidth Limit Exceeded",
+      510 => "Not Extended",
+      511 => "Network Authentication Required"
     }
 
     attr_reader :response
@@ -88,10 +88,10 @@ class Oauth2ApiClient
     # http error message.
 
     def self.const_name(message)
-      message.gsub(/[^a-zA-Z0-9]/, '')
+      message.gsub(/[^a-zA-Z0-9]/, "")
     end
 
-    STATUSES.each do |code, message|
+    STATUSES.each do |_code, message|
       const_set(const_name(message), Class.new(HttpError))
     end
   end
